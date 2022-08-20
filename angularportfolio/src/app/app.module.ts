c273d8b1ecec6a374fb0bf895fcf533c440ee364
juanpsosa <juanpsosa@users.noreply.github.com>
2022-08-19 23:30:42 -0300

    experiencia y estudios

--- angularportfolio/src/app/app.module.ts
+++ angularportfolio/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoapComponent } from './components/logoap/logoap.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,8 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
     LogoapComponent,
     BannerComponent,
     AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
   ],
   imports: [
     BrowserModule,
