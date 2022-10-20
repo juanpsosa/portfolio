999d39d984d2bbe3d2207d6e11aa2cebc2b63e6c
juanpsosa <59043056+juanpsosa@users.noreply.github.com>
2022-10-19 21:44:13 -0300

    BackEnd

---
+++ angularportfolio/BackEnd/src/main/java/com/portfolio/jps/Repository/IPersonaRepository.java
@@ -0,0 +1,11 @@
+
+package com.portfolio.jps.Repository;
+
+import com.portfolio.jps.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long>{
+    
+}
