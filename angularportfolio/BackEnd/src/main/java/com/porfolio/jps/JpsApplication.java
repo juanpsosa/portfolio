a5c1254838b57e4c4bc674a922ce57584fc5a2cf
juanpsosa <59043056+juanpsosa@users.noreply.github.com>
2022-10-25 21:05:51 -0300

    actualizacion springboot

---
+++ angularportfolio/BackEnd/src/main/java/com/porfolio/jps/JpsApplication.java
@@ -0,0 +1,13 @@
+package com.porfolio.jps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpsApplication.class, args);
+	}
+
+}
