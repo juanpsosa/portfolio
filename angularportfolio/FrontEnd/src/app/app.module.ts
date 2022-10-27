380c59fd9d7708686e95c22a5f8b95a9e993bfe5
juanpsosa <59043056+juanpsosa@users.noreply.github.com>
2022-10-26 23:28:20 -0300

    update Experiencia

--- angularportfolio/FrontEnd/src/app/app.module.ts
+++ angularportfolio/FrontEnd/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component'
 import { interceptorProvider } from './service/interceptor-service';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 
 @NgModule({
   declarations: [
@@ -36,6 +37,7 @@ import { NewExperienciaComponent } from './components/experiencia/new-experienci
     HomeComponent,
     LoginComponent,
     NewExperienciaComponent,
+    EditExperienciaComponent,
   ],
   imports: [
     BrowserModule,
