16ac66aa22c5349ea92148ba765ec04e9b2e0436
juanpsosa <59043056+juanpsosa@users.noreply.github.com>
2022-10-26 22:59:40 -0300

    componenteExperiencia

--- angularportfolio/FrontEnd/src/app/app.module.ts
+++ angularportfolio/FrontEnd/src/app/app.module.ts
@@ -18,6 +18,7 @@ import {HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component'
 import { interceptorProvider } from './service/interceptor-service';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 
 @NgModule({
   declarations: [
@@ -34,6 +35,7 @@ import { interceptorProvider } from './service/interceptor-service';
     AboutComponent,
     HomeComponent,
     LoginComponent,
+    NewExperienciaComponent,
   ],
   imports: [
     BrowserModule,
