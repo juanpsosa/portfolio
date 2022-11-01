a5ab0319a0153afe134d017f41c3a9d1bd21f339
juanpsosa <59043056+juanpsosa@users.noreply.github.com>
2022-10-31 23:11:06 -0300

    last update

--- angularportfolio/BackEnd/src/main/java/com/portfolio/jp/Controller/PersonaController.java
+++ angularportfolio/BackEnd/src/main/java/com/portfolio/jp/Controller/PersonaController.java
@@ -21,8 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins= "http://localhost:4200")
-//@CrossOrigin(origins = "https://frontend-jps.web.app")
+//@CrossOrigin(origins= "http://localhost:4200")
+@CrossOrigin(origins = "https://frontend-jps.web.app")
 public class PersonaController {
     @Autowired
     ImpPersonaService personaService;
